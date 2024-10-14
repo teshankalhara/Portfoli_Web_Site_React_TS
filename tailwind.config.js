dc9f2dcacc4714b3d8b17faeb4c5c292c6ed317d
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-10-15 02:24:52 +0530

    file_configuration_ok

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
