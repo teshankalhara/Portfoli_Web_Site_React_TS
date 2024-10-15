9b26bcd7469a17114f73bed6f8238604581670bf
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-10-16 02:36:48 +0530

    updtd_and_changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import './App.css'
 import Home from './pages/home/Home'
 import NavBar from './components/navbar/NavBar'
 import Contact from './pages/contact/Contact'
+import Project from './pages/project/Project'
+import Experience from './pages/experience/Experience'
+import Skill from './pages/skill/Skill'
+import About from './pages/about/About'
 
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/contact' element={<Contact />} />
+          <Route path='/project' element={<Project />} />
+          <Route path='/experience' element={<Experience />} />
+          <Route path='/skill' element={<Skill />} />
+          <Route path='/about' element={<About />} />
         </Routes>
       </BrowserRouter>
     </>
